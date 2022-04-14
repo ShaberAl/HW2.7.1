cd6dbda2003eb1de1c18381d14b6e0c6916e2634
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2022-04-14 15:53:09 +0500

    Initial Commit

---
+++ HW2.7.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.7.1
+//
+//  Created by MacOS on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
